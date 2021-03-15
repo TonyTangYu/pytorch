e4313b71dc3100461d30c4537f9d0b063fdd6ede
Marisa Kirisame <lolisa@marisa.moe>
2021-03-15 01:01:56 -0700

    save

--- aten/src/ATen/CheckpointTensorImpl.cpp
+++ aten/src/ATen/CheckpointTensorImpl.cpp
@@ -1,4 +1,6 @@
 #include <ATen/CheckpointTensorImpl.h>
+#include <ATen/core/op_registration/op_registration.h>
+#include <ATen/core/dispatch/Dispatcher.h>
 
 namespace at {
 
@@ -30,5 +32,16 @@ Tensor try_checkpoint(const Tensor& t) {
 }
 
 }
+
+void CheckpointFallback(const c10::OperatorHandle& op, torch::jit::Stack* stack) {
+}
+// todo: i can also use a torch library impl instead of calling fallback explicitly. should i do that?
+struct Register {
+  Register() {
+    static auto registration = c10::Dispatcher::singleton().registerFallback(DispatchKey::Checkpoint,
+                                                                             KernelFunction::makeFromBoxedFunction<&CheckpointFallback>(),
+                                                                             "checkpoint");
+  }
+} register_checkpoint;
 
 }
